f0108a2d431af7bbdadd25ababfd50e000d36348
Tristan Hamilton <hamilton@coli831.coli.uni-saarland.de>
2015-06-17 19:02:38 +0200

    format Wiki2Text

--- buildSrc/src/main/java/Wiki2Text.java
+++ buildSrc/src/main/java/Wiki2Text.java
@@ -1,54 +1,35 @@
-
 import java.io.*;
 
 import info.bliki.wiki.filter.PlainTextConverter;
 import info.bliki.wiki.model.WikiModel;
 
-import org.apache.commons.io.FileUtils;
-
 public class Wiki2Text {
 
-    public String root_dir;
-    public int page_id;
-    
-    public Wiki2Text(String area, String save_dir) {
-        root_dir = save_dir + "/" + area;
-        (new File(root_dir)).mkdirs();
-        page_id = 1;
+	public String root_dir;
+	public int page_id;
+
+	public Wiki2Text(String area, String save_dir) {
+		root_dir = save_dir + "/" + area + "/post";
+		(new File(root_dir)).mkdirs();
+		page_id = 1;
 	}
 
-    public void writeToFile(String wikiText)
-    {   
-        try
-        {
-            BufferedWriter out = new BufferedWriter(new FileWriter(root_dir + "/page_" + page_id + ".txt"));
-            out.write(wikiText);
-            out.close();
-        }
-        catch (IOException ex)
-        {
-            // FIXME: indicate the error!
-        }
-        finally
-        {
-            page_id++;
-        }
-    }
-    
-	public void getCleanText(File xml_file)
-    {
-        try
-        {
-        	WikiModel wikiModel = new WikiModel("http://www.mywiki.com/wiki/${image}", "http://www.mywiki.com/wiki/${title}");
-            String wikiText = FileUtils.readFileToString(xml_file);
-        	String plainStr = wikiModel.render(new PlainTextConverter(), wikiText);
+	private void writeToFile(String wikiText) {
+		try {
+			BufferedWriter out = new BufferedWriter(new FileWriter(root_dir + "/page_" + page_id + ".txt"));
+			out.write(wikiText);
+			out.close();
+		} catch (IOException ex) {
+			System.err.println("Caught IOException: " + ex.getMessage());
+		} finally {
+			page_id++;
+		}
+	}
 
-            // Write to file
-            writeToFile(plainStr);
-        }
-        catch(IOException e)
-        {
-            e.printStackTrace();
-        } 		
+	public void getCleanText(String xml_page) {
+			WikiModel wikiModel = new WikiModel("http://www.mywiki.com/wiki/${image}", "http://www.mywiki.com/wiki/${title}");
+			String plainStr = wikiModel.render(new PlainTextConverter(), xml_page);
+
+			writeToFile(plainStr);
 	}
 }

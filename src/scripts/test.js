badd861d5e125fd263d0278d4956736daf7374ad
tristan <hamiltt@tcd.ie>
2015-06-18 23:02:47 +0200

    update build script and remove no longer needed files. Still need task to download wikidumps, See TODO for ideas

--- src/scripts/test.js
+++
@@ -1,5 +0,0 @@
-var wiki = require('./txtwiki.js');
-var fs = require('fs');
-console.log(process.argv[2]);
-var file = fs.readFileSync(process.argv[2], "utf8");
-console.log(wiki.parseWikitext(file));
